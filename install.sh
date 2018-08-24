615177594d166a29bd92d7af8ef1b19105791607
Michal Koceba <michal.koceba@gmail.com>
2018-08-24 23:57:41 +0200

    Add font install and readme

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 ln -sv ~/.dotfiles/.gitconfig ~
 ln -sv ~/.dotfiles/.zshrc ~
